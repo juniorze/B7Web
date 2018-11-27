44486ceb1ce19a33bd50174e711f02c3f2690547
juniorze <junior.ze.jw@gmail.com>
2018-11-27 14:45:27 -0200

    Adicionando GitIgnore

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$nome = "Junior";	// String
+$idade = 90;		// Inteiro, Int, Integer
+$nota =  7.5;		// Float (numeros quebrados)
+$ligado = true;		// boolean TRUE ou FALSE
+$grupos = array(1, 2, 3); // arrey "Grupos de itens"
+
+echo $grupos[0];
+echo "<br>";
+echo $grupos[0] + $grupos[1] * $grupos[2];
+echo "<br>";
+print_r($grupos);
+?>
